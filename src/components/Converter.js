56ccb4c66036abfa263614528238d4518a13a91e
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-05-09 12:43:40 +0200

    The converter now works, the save and exchange units buttons are missing

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,85 +1,152 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import "../assets/styles/Converter.css";
 
+//!FALTA IMPLEMENTAR EL BOTÓN INTERCAMBIO
+//!FALTA IMPLEMENTAR EL BOTÓN GUARDAR
+
 const Converter = () => {
+  const [inputValue, setInputValue] = useState(0);
   const [displayUnit, setDisplayUnit] = useState("km");
   const [displayUnitConverted, setDisplayUnitConverted] = useState("miles");
-  const [inputValue, setInputValue] = useState(0);
   const [result, setResult] = useState(0);
+  const [selectedOption, setSelectedOption] = useState("");
 
-  
+  useEffect(() => {
+    const recalculateResult = () => {
+      const value = parseFloat(inputValue);
+
+      switch (document.querySelector("select").value) {
+        case "":
+          setDisplayUnit("");
+          setDisplayUnitConverted("");
+          break;
+        case "kmToMiles":
+          setDisplayUnit("km");
+          setDisplayUnitConverted("miles");
+          setResult(value * 0.621371 || 0);
+          break;
+        case "milesToKm":
+          setDisplayUnit("miles");
+          setDisplayUnitConverted("km");
+          setResult(value * 1.60934);
+          break;
+        case "feetToMeters":
+          setDisplayUnit("feet");
+          setDisplayUnitConverted("meters");
+          setResult(value * 0.3048);
+          break;
+        case "metersToFeet":
+          setDisplayUnit("meters");
+          setDisplayUnitConverted("feet");
+          setResult(value * 3.28084);
+          break;
+        case "cmToInches":
+          setDisplayUnit("cm");
+          setDisplayUnitConverted("inches");
+          setResult(value * 0.393701);
+          break;
+        case "inchesToCm":
+          setDisplayUnit("inches");
+          setDisplayUnitConverted("cm");
+          setResult(value * 2.54);
+          break;
+        default:
+          break;
+      }
+    };
+
+    recalculateResult();
+  }, [inputValue, selectedOption]);
 
   const handleSelectChange = (event) => {
-    const value = parseFloat(inputValue);
-
-    switch (event.target.value) {
-      case "kmToMiles":
-        setDisplayUnit("km");
-        setDisplayUnitConverted("miles");
-        setResult(inputValue * 0.621371).toFixed(2);
-        break;
-      case "milesToKm":
-        setDisplayUnit("miles");
-        setDisplayUnitConverted("km");
-        setResult(inputValue * 1.60934);
-        break;
-      case "feetToMeters":
-        setDisplayUnit("feets");
-        setDisplayUnitConverted("meters");
-        setResult(inputValue * 0.3048);
-        break;
-      case "metersToFeet":
-        setDisplayUnit("meters");
-        setDisplayUnitConverted("feets");
-        setResult(inputValue * 3.28084);
-        break;
-      case "cmToInches":
-        setDisplayUnit("cm");
-        setDisplayUnitConverted("inches");
-        setResult(inputValue * 0.393701);
-        break;
-      case "inchesToCm":
-        setDisplayUnit("inches");
-        setDisplayUnitConverted("cm");
-        setResult(inputValue * 2.54);
-        break;
-      default:
-        break;
-    }
+    setSelectedOption(event.target.value);
   };
 
+  // const handleUnitSwap = () => {
+  //   switch (selectedOption) {
+  //     case "kmToMiles":
+  //       setSelectedOption("milesToKm");
+  //       break;
+  //     case "milesToKm":
+  //       setSelectedOption("kmToMiles");
+  //       break;
+  //     case "feetToMeters":
+  //       setSelectedOption("metersToFeet");
+  //       break;
+  //     case "metersToFeet":
+  //       setSelectedOption("feetToMeters");
+  //       break;
+  //     case "cmToInches":
+  //       setSelectedOption("inchesToCm");
+  //       break;
+  //     case "inchesToCm":
+  //       setSelectedOption("cmToInches");
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // };
+
+  // const handleArrowButtonClick = () => {
+  //   handleUnitSwap();
+  // };
+
   const handleAutoConvert = (event) => {
-    setInputValue(event.target.value);
-    handleSelectChange({ target: { value: event.target.value}});  
+    const value = parseFloat(event.target.value) || 0;
+    setInputValue(value);
+  };
+
+  const handleClick = (event) => {
+    const value = parseFloat(event.target.value);
+    setInputValue(value);
   };
 
   return (
     <div className="converter">
-      <div className="title">convert</div>
+      <div className="title">Convert</div>
 
       <div className="content-select-icon-input">
         <select onChange={handleSelectChange}>
+          <option value="" hidden led selected>
+            Choise a option
+          </option>
           <option value="kmToMiles">km → miles</option>
           <option value="milesToKm">miles → km</option>
-          <option value="feetToMeters">feets → meters</option>
-          <option value="metersToFeet">meters → feets</option>
+          <option value="feetToMeters">feet → meters</option>
+          <option value="metersToFeet">meters → feet</option>
           <option value="cmToInches">cm → inches</option>
           <option value="inchesToCm">inches → cm</option>
         </select>
-        <FontAwesomeIcon className="convertIcon" icon={faArrowRightArrowLeft} />
-        <input type="number" value={inputValue} onChange={handleAutoConvert}/>
+
+        {/* <button onClick={handleArrowButtonClick}> */}
+        <button>
+          <FontAwesomeIcon
+            className="convertIcon-button"
+            icon={faArrowRightArrowLeft}
+          />
+        </button>
+        <input
+          type="number"
+          value={inputValue}
+          onClick={handleClick}
+          onChange={handleAutoConvert}
+        />
         <p>{displayUnit}</p>
       </div>
 
       <div className="content-button-result">
-        <button /*onClick={handleSaveConversion}*/>
-          <FontAwesomeIcon icon={faHeart} className="hearth"/>
+        <button>
+          <FontAwesomeIcon icon={faHeart} className="heart" />
         </button>
-        <div className="result"><strong>{result}</strong></div>
-        <div className="unitResult"><strong>{displayUnitConverted}</strong></div>
+        <div className="result">
+          <strong>{result}</strong>
+        </div>
+        <div className="unitResult">
+          <strong>{displayUnitConverted}</strong>
+        </div>
       </div>
     </div>
   );
